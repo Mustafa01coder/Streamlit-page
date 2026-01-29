3ade29b8c0d92ef20861f0181b74a7bc16e8c9bc
Mustafa01coder <165943191+Mustafa01coder@users.noreply.github.com>
2026-01-30 00:32:20 +0500

    Refactor Streamlit app structure and navigation

--- Assignment_No_7_MustafaAmjad.py
+++ Assignment_No_7_MustafaAmjad.py
@@ -68,11 +68,9 @@ elif page == 'Graphical Insights':
 
         st.bar_chart(profit_by_region)
 
-        # ---------------- GRAPH 3 ----------------
-        st.line_chart(df)
-
     else:
         st.warning('Please upload a dataset first in the Data Upload section.')
+
 
 
 
