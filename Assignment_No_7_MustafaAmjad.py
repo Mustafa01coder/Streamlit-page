278ba3f75f4e0ca3a73042bbe99604e7b3bec355
Mustafa01coder <165943191+Mustafa01coder@users.noreply.github.com>
2026-01-30 00:26:57 +0500

    Remove matplotlib import and fix minor typos

--- Assignment_No_7_MustafaAmjad.py
+++ Assignment_No_7_MustafaAmjad.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import seaborn as sns
 
 ## Creating the nagivation Bar
@@ -81,3 +81,4 @@ elif page == 'Graphical Insights':
 
     else:
         st.warning('Please upload a dataset first in the Data Upload section.')
+
